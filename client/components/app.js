7f81b19420341833838392bb5f7042c6d4eb8c84
Leo Gagnon <leognon@gmail.com>
2021-07-23 17:25:06 -0400

    Added p5-react example

--- client/components/app.js
+++ client/components/app.js
@@ -1,16 +1,30 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-//import Sketch from 'react-p5';
+import Sketch from 'react-p5';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
     }
 
+    setup(p5, canvasParentRef) {
+        p5.createCanvas(400, 400).parent(canvasParentRef);
+        p5.background(255,0,0);
+    }
+
+    draw(p5) {
+        p5.ellipse(p5.random(100), p5.random(200, 400), 50, 50);
+    }
+
+    keyPressed(p5) {
+        p5.background(Math.random()*255, Math.random()*255, Math.random()*255);
+    }
+
     render() {
         return (
             <div>
                 <h1>Hello world!</h1>
+                <Sketch setup={this.setup} draw={this.draw} keyPressed={this.keyPressed} />
             </div>
         );
     }
