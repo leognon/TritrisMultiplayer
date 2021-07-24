93353577191787f961ffb61709d90c031cc8ae24
Leo Gagnon <leognon@gmail.com>
2021-07-23 23:46:54 -0400

    Added grid, loading box and resizeCanvas

--- client/components/app.js
+++ client/components/app.js
@@ -1,33 +1,66 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import Sketch from 'react-p5';
+import states from '../../common/states.js';
+import Loading from './loading.js';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            state: states.LOADING
+        }
     }
 
     setup(p5, canvasParentRef) {
-        p5.createCanvas(400, 400).parent(canvasParentRef);
+        p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(canvasParentRef);
         p5.background(255,0,0);
     }
 
     draw(p5) {
-        p5.ellipse(p5.random(100), p5.random(200, 400), 50, 50);
+        p5.background(255,0,0);
     }
 
-    keyPressed(p5) {
-        p5.background(Math.random()*255, Math.random()*255, Math.random()*255);
+    windowResized(p5) {
+        p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
+        p5.redraw();
+    }
+
+    getUI() {
+        switch (this.state.state) {
+            case states.LOADING:
+                return <Loading />;
+            default:
+                console.log('State was ' + this.state.state);
+                return <h2>State: {this.state.state}</h2>;
+        }
     }
 
     render() {
         return (
-            <div>
-                <h1>Hello world!</h1>
-                <Sketch setup={this.setup} draw={this.draw} keyPressed={this.keyPressed} />
-            </div>
+            <>
+                <Sketch setup={this.setup} draw={this.draw} windowResized={this.windowResized} />
+                { this.getUI() }
+            </>
         );
     }
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
+/*
+App
+    Loading
+        Text in center saying loading
+    Home
+        Box in center
+            Title
+            Name input
+            Play buttons
+    Room
+        Lobby
+            Box in center
+                Room code
+                List of players
+        Ingame
+            Empty
+*/
---
+++ client/components/loading.js
@@ -0,0 +1,15 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+export default class Loading extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className="center box">
+                <h1>Loading...</h1>
+            </div>
+        );
+    }
+}
